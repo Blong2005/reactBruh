3adce7ecd0393a05be78fab38150d3db25ce38b8
Blong2005 <114321864+Blong2005@users.noreply.github.com>
2023-08-30 13:39:38 +0000

    bruh

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -2,9 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import TodoList from './components/TodoList.jsx'
+// to do list
 function App() {
-  return null
+  useState([])
+  return (
+    <>
+      <TodoList/>
+      <input type='text'/>
+      <button>poop</button>
+      <button>pee</button>
+      <button>poop again</button>
+      <div>0 left to do</div>
+    </>
+
+  )
 }
+ 
 
 export default App
+ 
