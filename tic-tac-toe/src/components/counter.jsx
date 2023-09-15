bcb1f4bc5fe0946ce47fa659858d00fd2bd5bfe8
Blong2005 <114321864+Blong2005@users.noreply.github.com>
2023-09-15 12:36:20 +0000

    poop

--- tic-tac-toe/src/components/counter.jsx
+++ tic-tac-toe/src/components/counter.jsx
@@ -1,23 +1,23 @@
-
+import { useState } from 'react';
 import Square from "./square.jsx";
 export default function Board() {
     return (
       <>
     
       <div className = "row">
-        <Square value = "1"/>
-        <Square value = "2"/>
-        <Square value = "3"/>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
       <div className = "row">
-        <Square value = "4"/>
-        <Square value = "5"/>
-        <Square value = "6"/>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
       <div className = "row">
-        <Square value = "7"/>
-        <Square value = "8"/>
-        <Square value = "9"/>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
       </>
     );
