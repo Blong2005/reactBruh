acef8b38f2dd91145a78f48c74fe678e833cde34
Blong2005 <114321864+Blong2005@users.noreply.github.com>
2023-09-18 13:23:52 +0000

    poop

--- tic-tac-toe/src/components/square.jsx
+++ tic-tac-toe/src/components/square.jsx
@@ -1,17 +1,12 @@
 import { useState } from 'react';
 
-export default function Square() {
-  const [value, setValue] = useState(null);
+export default function Square({ value, onSquareClick }) {
   
-    function handleClick() {
-      console.log('clicked!');
-      setValue('X');
-    }
   
     return (
       <button
         className="square"
-        onClick={handleClick}
+        onClick={onSquareClick}
       >
         {value}
       </button>
